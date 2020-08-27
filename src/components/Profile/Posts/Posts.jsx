932104ccc1208066cc6e5dce87895a3cd91ba26d
alextula26 <alex_marc86@mail.ru>
2020-08-27 23:18:57 +0300

    added refs in Profile components

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -3,14 +3,23 @@ import style from './Posts.module.css';
 import Post from './Post/Post.jsx';
 
 const Posts = ({ posts }) => {
+
+  const inputPost = React.createRef();
+  const textPost = React.createRef();
+
+  const addPost = () => {
+    alert(textPost.current.value + ' ' + inputPost.current.value);
+  };
+
   const postsList = posts.map(({name, age, message, like}) => <Post name={name} age={age} message={message} like={like}/>);
   return (
     <div className={style.profilePosts}>
       My posts
       <div className={style.postForm}>
-        <div><textarea/></div>
+        <div><input type="text" ref={inputPost} /></div>
+        <div><textarea ref={textPost} /></div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={style.posts}>
