e5a959cafad76e94a1a5dc5231bb1a6662881c9d
alextula26 <alex_marc86@mail.ru>
2020-09-01 00:07:28 +0300

    added store

--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import App from "./App";
 import './index.css';
-import state, {addPost, updateChangePost, subscribe} from "./state";
+import store from "./state";
 import * as serviceWorker from './serviceWorker';
 
 const render = (state) => {
   ReactDOM.render(
     <React.StrictMode>
       <App
+        key="app"
         state={state}
-        addPost={addPost}
-        updateChangePost={updateChangePost}/>
+        addPost={store.addPost.bind(store)}
+        updateChangePost={store.updateChangePost.bind(store)}/>
     </React.StrictMode>,
     document.getElementById('root')
   );
 };
 
-render(state);
+render(store.getState());
 
-subscribe(render);
+store.subscribe(render);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/state.js
+++ src/state.js
@@ -1,67 +1,72 @@
 import _ from 'lodash';
 
-let render = () => {};
-
-export const addPost = () => {
-
-  const ids = _.map(state.posts, (item) => item.id);
-  const id = _.last(ids) + 1;
-
-  const data = {
-    id,
-    name: state.profile.postForm.name,
-    age: state.profile.postForm.age,
-    message: state.profile.postForm.message,
-    like: 0,
-  };
-
-  state.profile.posts.push(data);
-
-  state.profile.postForm.name = '';
-  state.profile.postForm.age = '';
-  state.profile.postForm.message = '';
-
-  render(state);
-};
-
-export const updateChangePost = (name, age, message) => {
-
-  state.profile.postForm.name = name;
-  state.profile.postForm.age = age;
-  state.profile.postForm.message = message;
-
-  render(state);
-};
-
-export const subscribe = (observer) => {
-  render = observer;
-};
-
-const state = {
-  dialogs: [
-    {id: 1, name: 'Name 1'},
-    {id: 2, name: 'Name 2'},
-    {id: 3, name: 'Name 3'},
-    {id: 4, name: 'Name 4'},
-  ],
-  messages: [
-    {id: 1, text: 'Message1'},
-    {id: 2, text: 'Message2'},
-    {id: 3, text: 'Message3'},
-  ],
-  profile: {
-    posts: [
-      {id: 1, name: 'Alex', age: 30, message: 'How do you do?', like: 20},
-      {id: 2, name: 'Inna', age: 35, message: 'Cool!!!', like: 30},
-      {id: 3, name: 'Eva', age: 15, message: 'Way Cool!!!', like: 120},
+const store = {
+  _state: {
+    dialogs: [
+      {id: 1, name: 'Name 1'},
+      {id: 2, name: 'Name 2'},
+      {id: 3, name: 'Name 3'},
+      {id: 4, name: 'Name 4'},
     ],
-    postForm: {
-      name: '',
-      age: '',
-      message: '',
+    messages: [
+      {id: 1, text: 'Message1'},
+      {id: 2, text: 'Message2'},
+      {id: 3, text: 'Message3'},
+    ],
+    profile: {
+      posts: [
+        {id: 1, name: 'Alex', age: 30, message: 'How do you do?', like: 20},
+        {id: 2, name: 'Inna', age: 35, message: 'Cool!!!', like: 30},
+        {id: 3, name: 'Eva', age: 15, message: 'Way Cool!!!', like: 120},
+      ],
+      postForm: {
+        name: '',
+        age: '',
+        message: '',
+      },
     },
   },
+
+  getState() {
+    return this._state;
+  },
+
+  _callObserver() {},
+
+  addPost() {
+    const ids = _.map(this._state.posts, (item) => item.id);
+    const id = _.last(ids) + 1;
+
+    const data = {
+      id,
+      name: this._state.profile.postForm.name,
+      age: this._state.profile.postForm.age,
+      message: this._state.profile.postForm.message,
+      like: 0,
+    };
+
+    this._state.profile.posts.push(data);
+
+    this._state.profile.postForm.name = '';
+    this._state.profile.postForm.age = '';
+    this._state.profile.postForm.message = '';
+
+    this._callObserver(this._state);
+  },
+
+  updateChangePost (name, age, message) {
+
+    this._state.profile.postForm.name = name;
+    this._state.profile.postForm.age = age;
+    this._state.profile.postForm.message = message;
+
+    this._callObserver(this._state);
+  },
+
+  subscribe (observer) {
+    this._callObserver = observer;
+  }
 };
 
-export default state;
+export default store;
 
