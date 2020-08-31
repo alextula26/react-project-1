9d76f6b42fb7d8feaff6d0c0247e24799ed68746
alextula26 <alex_marc86@mail.ru>
2020-09-01 00:08:30 +0300

    state rename to store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import App from "./App";
 import './index.css';
-import store from "./state";
+import store from "./store";
 import * as serviceWorker from './serviceWorker';
 
 const render = (state) => {
